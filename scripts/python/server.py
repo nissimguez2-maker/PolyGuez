942228a14fad5f6d59a1f93de325eece39c8d5d9
s0wa48 <msmaciejsowinski@gmail.com>
2026-03-03 02:12:20 +0100

    refactor(deps): migrate fastapi from 0.111.0 to 0.135.1

--- scripts/python/server.py
+++ scripts/python/server.py
@@ -1,4 +1,3 @@
-from typing import Union
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -10,17 +9,17 @@ def read_root():
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
 
 
 @app.get("/trades/{trade_id}")
-def read_trade(trade_id: int, q: Union[str, None] = None):
+def read_trade(trade_id: int, q: str | None = None):
     return {"trade_id": trade_id, "q": q}
 
 
 @app.get("/markets/{market_id}")
-def read_market(market_id: int, q: Union[str, None] = None):
+def read_market(market_id: int, q: str | None = None):
     return {"market_id": market_id, "q": q}
 
 
